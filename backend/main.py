563424f7720e6d100908a8894a361de8baa2fec6
Survine <hridaybardhan18@gmail.com>
2025-08-21 20:55:42 +0530

    venv added

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from databases.database import Base, engine
 from Routes import medicine,order,customer,stock,user,invoice
 from fastapi.middleware.cors import CORSMiddleware
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title,
+    )
 origins = [
     "http://localhost:5173"
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import Customers from "./components/Customers"
 import Login from "./components/Login"
 import Medicines from "./components/Medicines"
 import Users from "./components/Users"
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 const App = () => {
   return (
     <div>
-      <Login />
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      {/* <Login />
       <Users />
       <Customers />
-      <Medicines />
+      <Medicines /> */}
     </div>
   )
 }
 export default App
+
+
+
